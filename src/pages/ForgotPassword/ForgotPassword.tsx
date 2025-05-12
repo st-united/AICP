5dbc0efaf18212ff6a95bc02c17a17d8ad2768a2
Spiderman62 <nguyenlamhoang7146@gmail.com>
2025-05-12 16:45:28 +0700

    chore: [AICP-9] Install lucide-react library

--- src/pages/ForgotPassword/ForgotPassword.tsx
+++ src/pages/ForgotPassword/ForgotPassword.tsx
@@ -1,10 +1,17 @@
+import { ArrowLeft } from 'lucide-react';
+
 import { InputField, Button } from '@app/components/ui/index';
 
 export default function ForgotPassword() {
   return (
     <div className='!p-12 flex justify-start h-full w-full'>
       <div className='w-full'>
-        <p className='text-white !mb-14'>Quay về trang đăng nhập</p>
+        <p className='text-white !mb-14 flex align-items gap-x-1'>
+          <span>
+            <ArrowLeft className='translate-y-[1.5px]' size={20} />
+          </span>
+          <span>Quay về trang đăng nhập</span>
+        </p>
         <div>
           <h1 className='text-4xl !text-white !font-bold'>Quên mật khẩu</h1>
           <p className='text-white !mb-8'>Bạn chưa có tài khoản? Đăng ký</p>
