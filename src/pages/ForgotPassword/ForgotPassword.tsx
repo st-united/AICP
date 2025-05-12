3fcacddc5242ceb3d34cf311b012b2f95223a272
Spiderman62 <nguyenlamhoang7146@gmail.com>
2025-05-12 20:38:33 +0700

    feat: [AICP-9] Implement i18n for forgot password

--- src/pages/ForgotPassword/ForgotPassword.tsx
+++ src/pages/ForgotPassword/ForgotPassword.tsx
@@ -1,8 +1,10 @@
 import { ArrowLeftOutlined } from '@ant-design/icons';
+import { useTranslation } from 'react-i18next';
 
 import { InputField, Button } from '@app/components/ui/index';
 
 export default function ForgotPassword() {
+  const { t } = useTranslation();
   return (
     <div className='!p-12 flex justify-start h-full w-full'>
       <div className='w-full'>
@@ -10,16 +12,18 @@ export default function ForgotPassword() {
           <span>
             <ArrowLeftOutlined className='-translate-y-[2px]' />
           </span>
-          <span>Quay về trang đăng nhập</span>
+          <span>{t('FORGOT_PASSWORD.TURN_BACK_SIGN_IN')}</span>
         </p>
         <div>
-          <h1 className='text-4xl !text-white !font-bold'>Quên mật khẩu</h1>
-          <p className='text-white !mb-8'>Bạn chưa có tài khoản? Đăng ký</p>
+          <h1 className='text-4xl !text-white !font-bold'>{t('FORGOT_PASSWORD.TITLE')}</h1>
+          <p className='text-white !mb-8'>
+            {t('FORGOT_PASSWORD.TITLE')} {t('FORGOT_PASSWORD.REGISTER')}
+          </p>
         </div>
         <form className='!space-y-8'>
           <InputField type={'email'} placeholder={'Email *'} />
           <Button type='primary' className={'w-full'}>
-            Đặt lại mật khẩu
+            {t('FORGOT_PASSWORD.RESET_PASSWORD')}
           </Button>
         </form>
       </div>
