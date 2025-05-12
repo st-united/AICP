ba64ed1f69fcaec741415e72636d2df720aaeddd
Spiderman62 <nguyenlamhoang7146@gmail.com>
2025-05-12 16:16:38 +0700

    feat: [AICP-9] Make auth layout responsive

---
+++ src/pages/ForgotPassword/ForgotPassword.tsx
@@ -0,0 +1,19 @@
+import { InputField, Button } from '@app/components/ui/index';
+
+export default function ForgotPassword() {
+  return (
+    <div className='!p-12 flex justify-start h-full w-full'>
+      <div className='w-full'>
+        <p className='text-white !mb-14'>Quay về trang đăng nhập</p>
+        <div>
+          <h1 className='text-4xl !text-white !font-bold'>Quên mật khẩu</h1>
+          <p className='text-white !mb-8'>Bạn chưa có tài khoản? Đăng ký</p>
+        </div>
+        <form className='!space-y-8'>
+          <InputField type={'email'} placeholder={'Email *'} />
+          <Button>Đặt lại mật khẩu</Button>
+        </form>
+      </div>
+    </div>
+  );
+}
