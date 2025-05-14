e535019a5db994184668c05aab118f4bbd05d7ad
Spiderman62 <nguyenlamhoang7146@gmail.com>
2025-05-14 09:21:35 +0700

    refactor: [AICP-43] Change the name

--- src/components/templates/PublicLayout/PublicLayout.tsx
+++ src/components/templates/PublicLayout/PublicLayout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
-const AuthLayout: React.FC = () => {
+const PublicLayout: React.FC = () => {
   return (
     <div className='h-screen w-full grid md:grid-cols-2 !p-10 bg-cover bg-center bg-no-repeat bg-[url(./assets/images/background.png)]'>
       <div className='sm:w-full md:w-6/6 lg:w-6/6 xl:w-5/6 h-full'>
@@ -15,4 +15,4 @@ const AuthLayout: React.FC = () => {
   );
 };
 
-export default AuthLayout;
+export default PublicLayout;
