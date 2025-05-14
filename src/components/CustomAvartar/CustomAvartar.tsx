27f9015a196d80fe7ee01ee0acaf5e7fa1dbf28e
bxthien <buixuanthien.0105@gmail.com>
2025-05-14 16:30:05 +0700

    feat: [AICP-24] update profile layout

--- src/components/CustomAvartar/CustomAvartar.tsx
+++ src/components/CustomAvartar/CustomAvartar.tsx
@@ -1,11 +1,20 @@
-import { CameraFilled } from '@ant-design/icons';
+import { CameraFilled, UserOutlined } from '@ant-design/icons';
 import { Avatar } from 'antd';
 
-const CustomAvartar = () => {
+interface Props {
+  avatar?: string;
+  isEdit?: boolean;
+}
+
+const CustomAvartar = ({ avatar, isEdit }: Props) => {
   return (
     <div className='relative'>
-      <Avatar className='relative !w-[180px] !h-[180px]' />
-      <div className='absolute bottom-0 right-2'>
+      <Avatar
+        className='relative md:!w-[180px] !w-[150px] md:!h-[180px] !h-[150px] !max-w-[900px]'
+        src={avatar}
+        icon={<UserOutlined className='md:!text-[180px] !text-[150px]' />}
+      />
+      <div className={`absolute bottom-0 right-2 cursor-pointer ${isEdit ? 'block' : 'hidden'}`}>
         <div className='flex items-center justify-center bg-[#3D6ADA] rounded-full !p-2'>
           <CameraFilled style={{ color: '#fff', fontSize: '24px' }} />
         </div>
