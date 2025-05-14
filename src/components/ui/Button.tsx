ef3ea7998786228e4bb0d8732a5be591373156b5
Spiderman62 <nguyenlamhoang7146@gmail.com>
2025-05-14 11:51:46 +0700

    feat: [AICP-9] refactor button and input field component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,13 +3,15 @@ interface ButtonProps {
   children: string;
   type?: 'primary' | 'default' | 'dashed' | 'text' | 'link' | 'ghost';
   className?: string;
+  disabled?: boolean;
 }
 
 export default function ButtonComponent({ ...props }: ButtonProps) {
-  const { children, type, className } = props;
+  const { children, type, className, disabled } = props;
   return (
     <Button
       type={type ? type : 'primary'}
+      disabled={disabled}
       htmlType='submit'
       className={`${className} !h-12 !rounded-md !font-medium`}
     >
--- src/components/ui/InputField.tsx
+++ src/components/ui/InputField.tsx
@@ -5,14 +5,17 @@ interface InputFieldProps {
   placeholder?: string;
   alt?: string;
   className?: string;
+  disabled?: boolean;
 }
 
 export default function InputField({ ...props }: InputFieldProps) {
-  const { type, placeholder, alt, className } = props;
+  const { type, placeholder, alt, className, disabled } = props;
   return (
     <Input
+      onInvalid={(e) => e.preventDefault()}
       type={type || 'text'}
       alt={alt}
+      disabled={disabled}
       className={`h-12 !bg-[#1955A0] !border-none !placeholder:text-[#69C0FF] !text-white !rounded-md ${
         className ? className : ''
       }`}
