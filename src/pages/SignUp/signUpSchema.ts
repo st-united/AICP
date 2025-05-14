0af8607c818c76ad5f48ab14785db03e33e41fc6
bxthien <buixuanthien.0105@gmail.com>
2025-05-14 12:27:50 +0700

    feat: [AICP-7] change using regex file

--- src/pages/SignUp/signUpSchema.ts
+++ src/pages/SignUp/signUpSchema.ts
@@ -1,28 +1,36 @@
 import { useTranslation } from 'react-i18next';
 import * as yup from 'yup';
 
+import {
+  EMAIL_REGEX_PATTERN,
+  PHONE_REGEX_PATTERN,
+  PASSWORD_REGEX_PATTERN,
+  NO_SPECIAL_CHARACTER_IN_NAME,
+} from '@app/constants/regex';
+
 export const useSignUpSchema = () => {
   const { t } = useTranslation();
 
   return yup.object().shape({
-    fullName: yup.string().required(t('VALIDATE.FULL_NAME_REQUIRED') as string),
+    fullName: yup
+      .string()
+      .required(t('VALIDATE.FULL_NAME_REQUIRED') as string)
+      .matches(NO_SPECIAL_CHARACTER_IN_NAME, t('VALIDATE.ONLY_ALPHABET') as string),
 
     phoneNumber: yup
       .string()
       .required(t('VALIDATE.PHONE_REQUIRED') as string)
-      .matches(/^\+?[0-9]{7,15}$/, t('VALIDATE.PHONE_INVALID') as string),
+      .matches(PHONE_REGEX_PATTERN, t('VALIDATE.PHONE_INVALID') as string),
 
     email: yup
       .string()
       .required(t('VALIDATE.EMAIL_REQUIRED') as string)
-      .email(t('VALIDATE.EMAIL_INVALID') as string),
+      .matches(EMAIL_REGEX_PATTERN, t('VALIDATE.EMAIL_INVALID') as string),
 
     password: yup
       .string()
       .required(t('VALIDATE.PASSWORD_REQUIRED') as string)
-      .min(8, t('VALIDATE.PASSWORD_MIN') as string)
-      .max(50, t('VALIDATE.PASSWORD_MAX') as string)
-      .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/, t('VALIDATE.PASSWORD_COMPLEXITY') as string),
+      .matches(PASSWORD_REGEX_PATTERN, t('VALIDATE.PASSWORD_COMPLEXITY') as string),
 
     confirm_password: yup.string().required(t('VALIDATE.CONFIRM_PASSWORD_REQUIRED') as string),
   });
