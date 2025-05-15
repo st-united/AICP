6fc8ac492b195b0deef6dd5ad6159322ff2b6477
Quy Pham <quy.pham@stunited.vn>
2025-05-15 21:44:58 +0700

    chore: fix authen

--- src/constants/navigate.ts
+++ src/constants/navigate.ts
@@ -5,7 +5,8 @@ export const NAVIGATE_URL = {
   USER_CREATE: '/users/create',
   PROFILE_UPDATE: '/profile/update',
   CHANGE_PASSWORD: '/profile/change-password',
-  SIGN_IN: '/sign-in',
+  SIGN_IN: '/login',
+  SIGN_UP: '/register',
   SIGN_OUT: '/sign-out',
   PROFILE: '/profile',
 };
