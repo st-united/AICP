33fc531a294fd9225d73da24e659172d65b6cb28
bxthien <buixuanthien.0105@gmail.com>
2025-05-15 11:12:38 +0700

    feat: [AICP-24] resolve conflict

--- src/pages/HomePage/Homepage.tsx
+++ src/pages/HomePage/Homepage.tsx
@@ -18,7 +18,6 @@ const Homepage = () => {
       >
         <div className='flex justify-center items-center flex-col gap-12 h-full !px-4'>
           <div className='lg:text-7xl md:text-5xl text-4xl max-w-[300px] md:max-w-full text-center leading-12 font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#C05604] via-[#E16100] to-[#FFF] !p-4'>
-<<<<<<< HEAD
             {t('HOMEPAGE.TITLE')}
           </div>
           <div className='lg:text-2xl md:text-lg text-lg text-white font-semibold !p-4 lg:max-w-[980px] md:max-w-[700px] text-center leading-10'>
@@ -26,15 +25,6 @@ const Homepage = () => {
           </div>
           <div className='!uppercase !rounded-full !md:py-4 !md:px-10 !py-3 !px-8 bg-white text-lg md:text-xl text-[#096DD9] font-bold hover:bg-[#096DD9] hover:text-white transition-all duration-300 ease-in-out'>
             {t('HOMEPAGE.BUTTON')}
-=======
-            {t<string>('HOMEPAGE.TITLE')}
-          </div>
-          <div className='lg:text-2xl md:text-lg text-lg text-white font-semibold !p-4 lg:max-w-[980px] md:max-w-[700px] text-center leading-10'>
-            {t<string>('HOMEPAGE.SUB_TITLE')}
-          </div>
-          <div className='!uppercase !rounded-full !md:py-4 !md:px-10 !py-3 !px-8 bg-white text-lg md:text-xl text-[#096DD9] font-bold hover:bg-[#096DD9] hover:text-white transition-all duration-300 ease-in-out'>
-            {t<string>('HOMEPAGE.BUTTON')}
->>>>>>> fb792f6 (feat: [AICP-24] update profile layout)
           </div>
         </div>
       </div>
