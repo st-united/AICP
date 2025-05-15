b9fc15dd52aeadce8bb4daf73ce2c7318419c6d8
Spiderman62 <nguyenlamhoang7146@gmail.com>
2025-05-15 08:28:29 +0700

    refactor: [AICP-9] remove on disabled for email

--- src/components/ui/InputField.tsx
+++ src/components/ui/InputField.tsx
@@ -12,11 +12,10 @@ export default function InputField({ ...props }: InputFieldProps) {
   const { type, placeholder, alt, className, disabled } = props;
   return (
     <Input
-      onInvalid={(e) => e.preventDefault()}
       type={type || 'text'}
       alt={alt}
       disabled={disabled}
-      className={`h-12 !bg-[#1955A0] !border-none !placeholder:text-[#69C0FF] !text-white !rounded-md ${
+      className={`h-12 !bg-[#1955A0] !border-none !placeholder:text-[#69C0FF] !text-white !rounded-md !pl-6 ${
         className ? className : ''
       }`}
       placeholder={placeholder}
