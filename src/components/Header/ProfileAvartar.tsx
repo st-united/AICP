89fb49001c75704b6dc200d6f1fd3fa2782dc0d5
bxthien <buixuanthien.0105@gmail.com>
2025-05-15 10:58:56 +0700

    feat: [AICP-24] fix comment & rebase

--- src/components/Header/ProfileAvartar.tsx
+++ src/components/Header/ProfileAvartar.tsx
@@ -1,34 +1,36 @@
 import { UserOutlined } from '@ant-design/icons';
 import { Avatar, Dropdown, Image } from 'antd';
-import { t } from 'i18next';
+import { useTranslation } from 'react-i18next';
 
 import { LogOut, ProfileGG } from '@app/assets/svgs';
 import type { MenuProps } from 'antd';
 
 import './ProfileAvatar.scss';
 
-const items: MenuProps['items'] = [
-  {
-    key: '1',
-    label: (
-      <div className='flex items-center gap-3 text-lg font-semibold !p-1 hover:!rounded-lg'>
-        <Image className='!w-8 !h-8' src={ProfileGG} />
-        <div>{t<string>('PROFILE.PERSONAL_PROFILE')}</div>
-      </div>
-    ),
-  },
-  {
-    key: '2',
-    label: (
-      <div className='flex items-center gap-3 text-lg font-semibold !p-1 text-[#ED0909]'>
-        <Image className='!w-8 !h-8' src={LogOut} />
-        <div>{t<string>('PROFILE.LOGOUT')}</div>
-      </div>
-    ),
-  },
-];
-
 const ProfileAvartar = () => {
+  const { t } = useTranslation();
+
+  const items: MenuProps['items'] = [
+    {
+      key: '1',
+      label: (
+        <div className='flex items-center gap-3 text-lg font-semibold !p-1 hover:!rounded-lg'>
+          <Image className='!w-8 !h-8' src={ProfileGG} />
+          <div>{t<string>('PROFILE.PERSONAL_PROFILE')}</div>
+        </div>
+      ),
+    },
+    {
+      key: '2',
+      label: (
+        <div className='flex items-center gap-3 text-lg font-semibold !p-1 text-[#ED0909]'>
+          <Image className='!w-8 !h-8' src={LogOut} />
+          <div>{t<string>('PROFILE.LOGOUT')}</div>
+        </div>
+      ),
+    },
+  ];
+
   return (
     <Dropdown menu={{ items }} placement='bottomRight' arrow>
       <Avatar
