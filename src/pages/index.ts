b660e113a8de04c7a32ab4dc6f2bc44f75ed530a
Spiderman62 <nguyenlamhoang7146@gmail.com>
2025-05-15 09:12:03 +0700

    feat: [AICP-9] add reset password page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,6 @@
 import NotFound from './NotFound/NotFound';
+import ResetPassword from './ResetPassword/ResetPassword';
 import SignIn from './SignIn/SignIn';
 import SignUp from './SignUp/SignUp';
 
-export { SignIn, NotFound, SignUp };
+export { SignIn, NotFound, ForgotPassword, ResetPassword };
