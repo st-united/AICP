f079ad256acea1b5df05ac850bd81d95b73ef859
Hoang Nguyen <nguyenlamhoang7146@gmail.com>
2025-05-17 20:45:19 +0700

    feat: [AICP-43] implement carousel for public layout

--- src/components/templates/PublicLayout/PublicLayout.tsx
+++ src/components/templates/PublicLayout/PublicLayout.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
+import PublicLayoutCarousel from './PublicLayoutCarousel';
+import {
+  devplus,
+  carousel_1,
+  carousel_2,
+  carousel_3,
+  carousel_4,
+} from '@app/assets/images/publicLayoutImages/index';
+
 const PublicLayout: React.FC = () => {
   return (
     <div className='min-h-screen grid grid-cols-1 lg:grid-cols-2 !p-10 bg-cover bg-center bg-no-repeat bg-[url(./assets/images/background.png)]'>
@@ -9,7 +18,21 @@ const PublicLayout: React.FC = () => {
       </div>
 
       <div className='flex justify-end'>
-        <div className='lg:block hidden w-full bg-white rounded-4xl sm:w-6/6 md:w-6/6 lg:w-6/6 xl:w-5/6 h-full'></div>
+        <div className='rounded-3xl p-12 overflow-hidden lg:block hidden w-full bg-white sm:w-6/6 md:w-6/6 lg:w-6/6 xl:w-5/6 h-full'>
+          <div className='flex flex-col justify-between h-full'>
+            <div className='h-[80px] w-[200px] flex items-start'>
+              <img src={devplus} className='w-full h-full' alt='dev plus' />
+            </div>
+            <PublicLayoutCarousel
+              autoplayOptions={{
+                delay: 4000,
+                stopOnInteraction: true,
+                stopOnMouseEnter: true,
+              }}
+              images={[carousel_1, carousel_2, carousel_3, carousel_4]}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
