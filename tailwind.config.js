236fd2de4eb645ee252f103c6f78a0b37543a62d
Hoang Nguyen <nguyenlamhoang7146@gmail.com>
2025-05-19 14:00:30 +0700

    feat: [AICP-49] Declare color global theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: 'var(--primary-color)',
+          light: 'var(--primary-color-light)',
+          bold: 'var(--primary-color-bold)',
+          gray: 'var(--primary-gray-color)',
+        },
+        paragraph: {
+          DEFAULT: 'var(--paragraph-color)',
+          light: 'var(--paragraph-light-color)',
+        },
+      },
+    },
   },
   plugins: [],
 };
