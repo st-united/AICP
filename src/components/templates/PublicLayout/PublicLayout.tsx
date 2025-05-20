f774595b7725fb17f1bf0857aa04c5b6add174e9
Hoang Nguyen <nguyenlamhoang7146@gmail.com>
2025-05-20 15:00:34 +0700

    feat: [AICP-43] implement public layout

--- src/components/templates/PublicLayout/PublicLayout.tsx
+++ src/components/templates/PublicLayout/PublicLayout.tsx
@@ -1,36 +1,18 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
 
-import PublicLayoutCarousel from './PublicLayoutCarousel';
-import {
-  devplus,
-  carousel_1,
-  carousel_2,
-  carousel_3,
-  carousel_4,
-} from '@app/assets/images/publicLayoutImages/index';
+import { SlideImages } from '../../molecules/index';
 
 const PublicLayout: React.FC = () => {
   return (
-    <div className='min-h-screen grid grid-cols-1 lg:grid-cols-2 !p-10 bg-cover bg-center bg-no-repeat bg-[url(./assets/images/background.png)]'>
-      <div className='pt-6 sm:p-6 md:p-10 lg:p-14 xl:p-20'>
+    <div className='min-h-screen grid grid-cols-1 lg:grid-cols-2 bg-cover bg-center bg-no-repeat bg-[url(./assets/images/SlideImages/background.png)]'>
+      <div className='pt-12 p-6 sm:p-6 sm:pt-12 md:p-10 lg:p-14 xl:p-20'>
         <Outlet />
       </div>
 
-      <div className='flex justify-end'>
-        <div className='rounded-3xl p-12 overflow-hidden lg:block hidden w-full bg-white sm:w-6/6 md:w-6/6 lg:w-6/6 xl:w-5/6 h-full'>
+      <div className='flex justify-end max-h-screen p-10'>
+        <div className='shadow-md rounded-3xl p-12 overflow-hidden lg:block hidden w-full bg-white sm:w-6/6 md:w-6/6 lg:w-6/6 xl:w-5/6 max-h-screen'>
           <div className='flex flex-col justify-between h-full'>
-            <div className='h-[80px] w-[200px] flex items-start'>
-              <img src={devplus} className='w-full h-full' alt='dev plus' />
-            </div>
-            <PublicLayoutCarousel
-              autoplayOptions={{
-                delay: 4000,
-                stopOnInteraction: true,
-                stopOnMouseEnter: true,
-              }}
-              images={[carousel_1, carousel_2, carousel_3, carousel_4]}
-            />
+            <SlideImages />
           </div>
         </div>
       </div>
