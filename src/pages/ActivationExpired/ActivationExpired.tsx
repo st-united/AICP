05477ed63d9fa5c1422eb06215f465fa5d143057
Dong-Ka <dong.kaphu@stunited.vn>
2025-07-16 08:58:05 +0700

    feat: [AICPS-48] fix how to get email

--- src/pages/ActivationExpired/ActivationExpired.tsx
+++ src/pages/ActivationExpired/ActivationExpired.tsx
@@ -1,6 +1,6 @@
 import { LeftOutlined } from '@ant-design/icons';
 import { Form } from 'antd';
-import React from 'react';
+import { useEffect } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 import { Link, useSearchParams } from 'react-router-dom';
 
@@ -15,7 +15,7 @@ export default function ActivationExpired() {
 
   const email = searchParams.get('email') || '';
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (email) {
       form.setFieldsValue({ email });
     }
@@ -30,18 +30,20 @@ export default function ActivationExpired() {
       <div className='w-full'>
         <Link
           to='/login'
-          className='mb-12 font-medium flex items-center gap-x-1 text-primary-gray text-lg hover:text-primary'
+          className='mb-12 font-medium flex items-center gap-x-1 text-primary-gray text-base sm:text-lg md:text-xl hover:text-primary'
         >
           <LeftOutlined size={24} />
           <span>{t('ACTIVATION_EXPIRED.TURN_BACK_SIGN_IN')}</span>
         </Link>
 
         <div className='flex flex-col gap-y-4'>
-          <h1 className='text-3xl sm:text-3xl md:text-4xl lg:text-4xl text-primary font-bold'>
+          <h1 className='text-3xl sm:text-4xl md:text-4xl lg:text-[40px] text-primary font-bold'>
             {t('ACTIVATION_EXPIRED.TITLE')}
           </h1>
-          <p className='text-primary-gray text-lg'>{t('ACTIVATION_EXPIRED.DESCRIPTION_1')}</p>
-          <p className='text-primary-gray text-lg'>
+          <p className='text-primary-gray text-base sm:text-lg md:text-xl'>
+            {t('ACTIVATION_EXPIRED.DESCRIPTION_1')}
+          </p>
+          <p className='text-primary-gray text-base sm:text-lg md:text-xl'>
             <Trans
               i18nKey='ACTIVATION_EXPIRED.DESCRIPTION_2'
               components={{ strong: <strong className='font-bold' /> }}
@@ -49,19 +51,19 @@ export default function ActivationExpired() {
           </p>
         </div>
         <div className='max-w-md mx-auto mt-[40px] flex flex-col gap-y-[40px]'>
-          <div>
+          <Form form={form} onFinish={onFinish} className='flex flex-col gap-y-[40px]'>
             <Form.Item name={'email'} className='!mb-0'>
               <InputField
                 disabled={true}
                 type={'text'}
                 placeholder={email || 'Email *'}
-                className='text-[1rem] placeholder:text-[1rem]'
+                className='text-sm sm:text-base md:text-lg placeholder:text-sm sm:placeholder:text-base md:placeholder:text-lg !px-6 !py-4 !rounded-md'
               />
             </Form.Item>
-          </div>
-          <Form form={form} onFinish={onFinish}>
-            <Button type='primary' className={'w-full'} loading={isLoading}>
-              <span className='font-bold text-[1rem]'>{t('ACTIVATION_EXPIRED.RESEND_EMAIL')}</span>
+            <Button type='primary' className={'w-full, !h-[60px]'} loading={isLoading}>
+              <span className='font-bold text-sm sm:text-base md:text-lg'>
+                {t('ACTIVATION_EXPIRED.RESEND_EMAIL')}
+              </span>
             </Button>
           </Form>
         </div>
