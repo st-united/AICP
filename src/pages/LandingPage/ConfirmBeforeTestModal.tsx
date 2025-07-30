42752f0e57043f9703d317a832eda9a0821cca2d
st-Aminh <minh.le@stunited.vn>
2025-07-30 11:44:00 +0700

    fix: [AICPS-50-dev] use new before tests modal

--- src/pages/LandingPage/ConfirmBeforeTestModal.tsx
+++ src/pages/LandingPage/ConfirmBeforeTestModal.tsx
@@ -1,23 +1,12 @@
-import { Button } from 'antd';
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
-import { ImproveTestModal } from './Modals/ImproveTestModal';
-import { InfoModal } from './Modals/InfoModal';
-import { NewTestModal } from './Modals/NewTestModal';
 import { ContinueTestModal } from './Modals/ContinueTestModal';
-
+import { ImproveTestModal } from './Modals/ImproveTestModal';
+import { NewTestModal } from './Modals/NewTestModal';
 import { Modal } from '@app/components/molecules';
 import { NAVIGATE_URL } from '@app/constants';
-import {
-  useHasTakenExamDefault,
-  useSubmitExam,
-  useUpdateUserStudentInfo,
-  useGetHistory,
-} from '@app/hooks';
-import { RootState } from '@app/redux/store';
+import { ExamStatusEnum } from '@app/constants/enum';
+import { useHasTakenExamDefault, useSubmitExam, useGetHistory } from '@app/hooks';
 
 interface ConfirmBeforeTestModalProps {
   open: boolean;
@@ -25,12 +14,9 @@ interface ConfirmBeforeTestModalProps {
 }
 
 export default function ConfirmBeforeTestModal({ open, onClose }: ConfirmBeforeTestModalProps) {
-  const { t } = useTranslation();
   const navigate = useNavigate();
 
-  const { mutate: submitExam, isPending } = useSubmitExam();
-  const { mutate: updateUserStudentInfo } = useUpdateUserStudentInfo();
-  const { user } = useSelector((state: RootState) => state.auth);
+  const { mutate: submitExam } = useSubmitExam();
   const { data: hasTakenExam } = useHasTakenExamDefault();
   const { data: historyData } = useGetHistory();
 
@@ -38,7 +24,9 @@ export default function ConfirmBeforeTestModal({ open, onClose }: ConfirmBeforeT
   const handleReviewResult = () => navigate(NAVIGATE_URL.TEST_RESULT);
 
   const renderModalContent = () => {
-    const inProgressExam = historyData?.find((item) => item.examStatus === 'IN_PROGRESS');
+    const inProgressExam = historyData?.find(
+      (item) => item.examStatus === ExamStatusEnum.IN_PROGRESS,
+    );
     if (inProgressExam) {
       return (
         <ContinueTestModal
@@ -65,7 +53,6 @@ export default function ConfirmBeforeTestModal({ open, onClose }: ConfirmBeforeT
     return (
       <NewTestModal
         confirmProps={{ onClose }}
-        handleBackInfo={() => {}}
         handleStartTest={handleStartTest}
         hasTakenExam={hasTakenExam}
       />
