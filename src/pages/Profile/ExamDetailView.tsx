a2bd18b627454d0379db531a80cdf4bfbcbe48d7
Dang Van Sinh <sinh.dang@stunited.vn>
2025-08-04 14:43:33 +0700

    fix: [AICPS-138] adjust CompetencyChart width for improved responsiveness

--- src/pages/Profile/ExamDetailView.tsx
+++ src/pages/Profile/ExamDetailView.tsx
@@ -114,7 +114,7 @@ const ExamDetailView = ({ exam, onBack }: ExamDetailViewProps) => {
               </div>
             </div>
             <div className='w-full overflow-x-auto'>
-              <div className='min-w-[1000px] h-full'>
+              <div className='min-w-[62.5rem] h-full'>
                 <CompetencyChart data={dataChart} />
               </div>
             </div>
